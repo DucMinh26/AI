82d26cade16f53cc55a5e29bb3b98e7db9899dbc
2221050451-NghiemDucMinh <minh2632004@gmail.com>
2026-03-08 23:54:41 +0700

    NGÀY 57: MẠNG NƠ-RON NHÂN TẠO (NEURAL NETWORKS) - BỘ NÃO CỦA MÁY TÍNH

--- Jarvis_Project/notebooks/predict.py
+++ Jarvis_Project/notebooks/predict.py
@@ -10,9 +10,15 @@ try:
     load_model = joblib.load(model_path)
     print("--- TRIỆU HỒI BỘ NÃO AI THÀNH CÔNG ---")
 
-    kh_moi = pd.DataFrame([[30, 15000]], columns=['Tuổi', 'Lương ($)'])
+    kh_moi = pd.DataFrame([[18, 1000]], columns=['Tuổi', 'Lương ($)'])
     kq = load_model.predict(kh_moi)
     print(f"Kết quả dự đoán: {'Mua xe' if kq[0] == 1 else 'Không mua xe'}")
 
 except FileNotFoundError:
     print(f"Không tìm thấy file mô hình tại {model_path}. Vui lòng đảm bảo rằng file model_du_doan_mua_xe.pkl đã tồn tại.")
+
+# Thay vì chỉ dùng .predict(), ta dùng .predict_proba()
+phan_tram = load_model.predict_proba(kh_moi)
+
+# phan_tram sẽ trả về: [[xác suất 0, xác suất 1]]
+print(f"Độ tự tin: {phan_tram[0][1] * 100:.2f}%")
